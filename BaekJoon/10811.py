a4f22206d8d50df6d26d12b5e7eba6f9b53bdf1d
Deep-of-Machine <tjgus5569@hanyang.ac.kr>
2023-08-14 18:18:44 +0900

    2023/08/14

--- BaekJoon/10811.py
+++ BaekJoon/10811.py
@@ -1,15 +1,25 @@
 n, m = map(int, input().split())
 
 b = [1 * i for i in range(1, n+1)]
-
-l = []
-
-for i in range(m):
-    l=[]
-    q,w = map(int, input().split())
-    if q > w:
-        print(b[w:q+1])
-    elif q < w :
-        print(b[q:w+1])
+c = []
+for k in range(m):
+    i, j = map(int, input().split())
+    if i < j:
+        for l in range(j-i+1):
+            c.append(b[i-1])
+            b.pop(i-1)
+        for l in range(j-i+1):
+            b.insert(i-1, c[0])
+            c.pop(0)
+    if i > j:
+        for l in range(i-j+1):
+            c.append(b[j-1])
+            b.pop(j-1)
+        for l in range(i-j+1):
+            b.insert(j-1, c[0])
+            c.pop(0)
     else:
         pass
+
+for i in b:
+    print(i, end=" ")
